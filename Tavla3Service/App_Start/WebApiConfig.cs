bee992f10b3f48a26b39c8eefac131f8583f2f38
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2015-02-09 18:55:08 +0100

    initial from template

---
+++ Tavla3Service/App_Start/WebApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using Tavla3Service.DataObjects;
+using Tavla3Service.Models;
+
+namespace Tavla3Service
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            
+            Database.SetInitializer(new Tavla3Initializer());
+        }
+    }
+
+    public class Tavla3Initializer : ClearDatabaseSchemaIfModelChanges<Tavla3Context>
+    {
+        protected override void Seed(Tavla3Context context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
---
+++ Tavla3Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tavla3Service.WebApiApplication" Language="C#" %>
